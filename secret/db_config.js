03e0ec1cd0b8d242c275cc6f6eceb418dd518288
Hwan Oh <hwangoon@Hwanui-MacBook.local>
2018-08-11 20:32:38 +0900

    보안파일 삭제

--- secret/db_config.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    host: 'lab543.cqtvw7ehdspn.ap-northeast-2.rds.amazonaws.com',
-    port: 3306,
-    user: 'work543',
-    password: 'Work543@Amazon!',
-    database: 'work543'
-};
--- secret/jwt_config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    // 토큰을 인코딩/디코딩하기 위한 비밀키로 사용된다.
-    jwtSecret: 'Work543@Work543',
-    // Passport 세션을 이용하지 않도록 알려주는 역할을 한다.
-    jwtSession: {
-        session: false
-    }
-};
